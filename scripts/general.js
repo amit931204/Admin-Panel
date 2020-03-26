0ebd6c36aad1095c8eb37eed2af755a2c35c7028
Amit Chaurasiya <amit.bhumca15@gmail.com>
2020-03-27 01:25:59 +0530

    index.html and index.css

---
+++ scripts/general.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//   $("a.mobile").click(function() {
+//     $(".sidebar").slidetoggle("fast");
+//   });
+//   window.onrsize = function(event) {
+//     alert($(window).width());
+//   };
+// });
